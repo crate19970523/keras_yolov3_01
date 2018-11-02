b12354d35b0778b369628038c916f6d0df72dd1b
s19970523s <s19970523s@gmail.com>
2018-11-02 09:48:57 +0800

    'trainFinsh'

--- yolo.py
+++ yolo.py
@@ -20,13 +20,13 @@ from keras.utils import multi_gpu_model
 
 class YOLO(object):
     _defaults = {
-        "model_path": 'model_data/yolo.h5',
+        "model_path": 'logs/000/trained_weights_final.h5',
         "anchors_path": 'model_data/yolo_anchors.txt',
-        "classes_path": 'model_data/coco_classes.txt',
+        "classes_path": 'model_data/my_classes.txt',
         "score" : 0.3,
         "iou" : 0.45,
         "model_image_size" : (416, 416),
-        "gpu_num" : 1,
+        "gpu_num" : 0,
     }
 
     @classmethod
@@ -210,3 +210,15 @@ def detect_video(yolo, video_path, output_path=""):
             break
     yolo.close_session()
 
+if __name__ == '__main__':
+    yolo=YOLO()
+    path = '/imgtest/1.jpg'
+    try:
+        image = Image.open(path)
+    except:
+        print('Open Error! Try again!')
+    else:
+        r_image, _ = yolo.detect_image(image)
+        r_image.show()
+
+    yolo.close_session()
